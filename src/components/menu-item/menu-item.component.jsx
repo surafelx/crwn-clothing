3fd1f229c1acb478b3b018f9a2fa0cf5ba3d3d06
Surafel Yimam <surafelyimam.sy@gmail.com>
2021-03-26 09:42:38 +0300

    Updated menu-items to get history prop and match navigation with linkUrl

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
   <div className={`${size} menu-item`}>
     <div
       className="background-image"
       style={{
         backgroundImage: `url(${imageUrl})`,
       }}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
     ></div>
 
     <div className="content">
@@ -17,4 +19,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
